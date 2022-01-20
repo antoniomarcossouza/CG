733f576a7c9e9869790256048cf81a3e1d871b83
Antônio Marcos <antoniomarcos.souza2002@gmail.com>
2022-01-20 11:59:34 -0300

    alterações nos títulos da sidebar

--- html/resources/list.js
+++ html/resources/list.js
@@ -1,106 +1,108 @@
 var list = {
 	"en": {
-    "Trabalhos": {
-      "Trabalho 01": {
-        "Trabalho 01": "works/trabalho01",
-      },
-      "Trabalho 02": {
-        "Trabalho 02": "works/trabalho02",
-      },
-    },
-    "Exercícios": {
-      "Aula 01": {
-        "Exercício 01": "works/aula01_ex01",
-        "Exercício 02": "works/aula01_ex02",
-        "Exercício 03": "works/aula01_ex03",
-      },
-      "Aula 02": {
-        "Exercício 01": "works/aula02_ex01",
-        "Exercício 02": "works/aula02_ex02",
-      },
-      "Aula 03": {
-        "Exercício 01": "works/aula03_ex01",
-      },
-      "Aula 04": {
-        "Exercício 01": "works/aula04_ex01",
-        "Exercício 02": "works/aula04_ex02",
-      },
-      "Aula 05": {
-        "Exercício 01": "works/aula05_ex01",
-      },
-      "Aula 06": {
-        "Exercício 01": "works/aula06_ex01",
-        "Exercício 02": "works/aula06_ex02",
-      },
-    },
+		"Atividades": {
+			"Introdução ao Three.js": {
+				"Exercício 01": "works/aula01_ex01",
+				"Exercício 02": "works/aula01_ex02",
+				"Exercício 03": "works/aula01_ex03",
+			},
+			"Transformações Geométricas": {
+				"Exercício 01": "works/aula02_ex01",
+				"Exercício 02": "works/aula02_ex02",
+			},
+			"Projeções e Câmera virtual": {
+				"Exercício 01": "works/aula03_ex01",
+			},
+			"Animações": {
+				"Exercício 01": "works/aula04_ex01",
+				"Exercício 02": "works/aula04_ex02",
+			},
+			"T1": {
+				"Trabalho 01": "works/trabalho01",
+			},
+			"Iluminação": {
+				"Exercício 01 (Incompleto)": "works/aula05_ex01",
+			},
+			"Representações de objetos 3D": {
+				"Exercício 01": "works/aula06_ex01",
+				"Exercício 02": "works/aula06_ex02",
+			},
+			"T2": {
+				"Trabalho 02": "works/trabalho02",
+			},
+			"Texturas":{
+				"Exercício 01": "works/aula07_ex01",
+				"Exercício 02": "works/aula07_ex02",
+			}
+		},
 		"Projects": {
-			"Basics":  {
+			"Basics": {
 				"Basic Scene": "html/basicSceneInfo",
 				"Using the keyboard": "html/keyboardExampleInfo",
 				"Geometric Transformations 1": "html/geometricTransformationInfo",
 				"Geometric Transformations 2": "html/geometricTransformation2Info",
 				"Projection": "html/projectionInfo",
-				"Virtual Camera": "html/virtualCameraInfo",				
+				"Virtual Camera": "html/virtualCameraInfo",
 			},
-			"Animations":  {
+			"Animations": {
 				"Basic": "html/animationInfo",
 				"Using External Objects": "html/animationExternalObjectsInfo",
 			},
-			"Lighting":  {
+			"Lighting": {
 				"Types of lights": "html/lightingTypesInfo",
 				"Types of materials": "html/lightingMaterialsInfo",
-				"Spotlight in Details": "html/lightingSpotlightDetailsInfo",				
+				"Spotlight in Details": "html/lightingSpotlightDetailsInfo",
 			},
-			"Shadows":  {
-				"Shadow Map Types":"html/shadowMapTypesInfo",			
-				"Shadow Map Viewer":"html/shadowMapViewerInfo",
-				"Shadow Update":"html/shadowUpdateInfo",				
-			},			
-			"Geometries":  {
-				"Manual Creation": "html/geometriesManualInfo",				
+			"Shadows": {
+				"Shadow Map Types": "html/shadowMapTypesInfo",
+				"Shadow Map Viewer": "html/shadowMapViewerInfo",
+				"Shadow Update": "html/shadowUpdateInfo",
+			},
+			"Geometries": {
+				"Manual Creation": "html/geometriesManualInfo",
 				"Basic": "html/geometriesBasicInfo",
 				"Lathe": "html/geometriesLatheInfo",
 				"Extrusion": "html/geometriesExtrusionInfo",
 				"Convex": "html/geometriesConvexInfo",
 				"Loading External Files": "html/geometriesExternalObjectsInfo",
-			},			
-			"Textures":  {
+			},
+			"Textures": {
 				"Basic": "html/textureBasicInfo",
 				"Main Options": "html/textureOptionsInfo",
 				"Textured External Objects": "html/textureExternalObjectsInfo",
 			},
-			"Raytracing":  {
+			"Raytracing": {
 				"Example 1": "html/raytracingExample1Info",
 				"Example 2": "html/raytracingExample2Info",
-			},			
+			},
 		},
 		"XR": {
-			"AR & VR with WebXR":  {
-				"VR - Panorama": "html/webxr_VR_PanoramaInfo",				
+			"AR & VR with WebXR": {
+				"VR - Panorama": "html/webxr_VR_PanoramaInfo",
 				"VR - Teleport": "html/webxr_VR_TeleportInfo",
-				"VR - Free Move": "html/webxr_VR_FreeMoveInfo",				
-				"VR - Dragging": "html/webxr_VR_DraggingInfo",				
-				"VR - Labelling":"html/webxr_VR_LabellingInfo",
-				"AR - Hit and Build": "html/webxr_AR_HitAndBuildInfo",				
+				"VR - Free Move": "html/webxr_VR_FreeMoveInfo",
+				"VR - Dragging": "html/webxr_VR_DraggingInfo",
+				"VR - Labelling": "html/webxr_VR_LabellingInfo",
+				"AR - Hit and Build": "html/webxr_AR_HitAndBuildInfo",
 			},
-			"AR & VR with external libs":  {
+			"AR & VR with external libs": {
 				"VR - Primitives": "html/VR_BasicInfo",
 				"VR - Panorama": "html/VR_PanoramaInfo",
 				"VR - External Models": "html/VR_ExternalModelInfo",
-				"AR - Fiducial Marker": "html/AR_BasicInfo",	
+				"AR - Fiducial Marker": "html/AR_BasicInfo",
 			},
 		},
-		"Additional Examples":{
-			"Physics":  {
+		"Additional Examples": {
+			"Physics": {
 				"Ammo - Vehicle": "html/ammoVehicleInfo",
 				"Ammo - Breakable Objects": "html/ammoBreakableObjectsInfo",
 			},
-			"Misc":  {
+			"Misc": {
 				"Group Geometry": "html/exampleGroupInfo",
 				"First Person": "html/exampleFirstPersonInfo",
 				"Drag Control": "html/exampleDragControlInfo",
-				"Fly Controls":"html/exampleFlyControlInfo",		
-				"Joystick Elements":"html/exampleJoystickInfo",						
+				"Fly Controls": "html/exampleFlyControlInfo",
+				"Joystick Elements": "html/exampleJoystickInfo",
 				"Audio": "html/exampleAudioInfo",
 			},
 		},
